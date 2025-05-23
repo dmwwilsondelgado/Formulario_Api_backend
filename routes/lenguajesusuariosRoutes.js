ab03658cb394fc161fd77e04f46f40854ca2f89c
Wili Delgado <wilsondelgadomoreno@gmail.com>
2025-05-22 23:33:54 -0500

    creamos el crud con sus respectuvas routes y controllador

--- routes/lenguajesusuariosRoutes.js
+++ routes/lenguajesusuariosRoutes.js
@@ -1,5 +1,16 @@
 import express from "express";
-//
-//
-const router = express.router();
-module.exports = router;
+import Lenguajes_Usuarios from "../controller/lenguajesusuariosController.js";
+
+const router = express.Router();
+
+router.get('/', Lenguajes_Usuarios.getAllLenguajesUsuarios);
+
+router.post('/', Lenguajes_Usuarios.createLenguajesUsuarios);
+
+router.put('/:id', Lenguajes_Usuarios.updateLenguajesUsuarios);
+
+router.patch('/:id_usuario', Lenguajes_Usuarios.updateParcialLenguajesUsuarios);
+
+router.delete('/:id_usuario', Lenguajes_Usuarios.deleteLenguajesUsuarios);
+
+export default router;
