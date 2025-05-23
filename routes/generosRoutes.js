330cfef82106f811e2301f50cb05afe987088b7d
Wili Delgado <wilsondelgadomoreno@gmail.com>
2025-05-22 22:23:54 -0500

    finalizamos de crear la ruta genero y su respectivo controllador

--- routes/generosRoutes.js
+++ routes/generosRoutes.js
@@ -1,5 +1,17 @@
 import express from "express";
-//
-//
-const router = express.router();
-module.exports = router;
+import GenerosController from "../controller/generosController";
+
+const router = express.Router();
+
+router.get('/', GenerosController.getAllGeneros);
+
+router.post('/', GenerosController.createGeneros);
+
+router.put('/:id_genero', GenerosController.updateGeneros);
+
+router.patch('/:id_genero', GenerosController.updateParcialGeneros);
+
+router.delete('/:id_genero', GenerosController.deleteGeneros);
+
+
+export default router;
