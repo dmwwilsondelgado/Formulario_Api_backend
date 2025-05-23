14698f693576be45e9a223996fe17592ba7fce52
Wilson Delgado <wilsondelgadomoreno@gmail.com>
2025-05-23 10:31:53 -0500

    get ciudades post hechas corretamente el crud andamos validando los campos

--- services/ciudadService.js
+++ services/ciudadService.js
@@ -1,14 +1,15 @@
 import Ciudades from "../models/ciudades.js";
 class CiudadService {
-  static async getCiudades() {
+  static async getCiudades()
+  {
     try {
       const ciudadInstance = new Ciudades();
       const ciudades = await ciudadInstance.getAll();
-      if (ciudades.length === 1) {
+      if (ciudades.length === 0) {
         return {
           error: true,
           code: 404,
-          message: "NO HAY CIUDADES REGISTRADAS",
+          message: "NO HAY CIUDADES REGISTRADAS"
         };
       }
       return {
@@ -26,13 +27,13 @@ class CiudadService {
       };
     }
   }
-  static async getCityById(id)
+  static async getCiudadById(id)
   {
     try {
-      const ciudadInstance = new Ciudad();
-      const ciudad = await ciudadInstance.getById(id);
+      const ciudadInstance = new Ciudades();
+      const ciudades = await ciudadInstance.getById(id);
       // Validamos si no hay ciudades
-      if (ciudad.length === 0) {
+      if (ciudades.length === 0) {
         return {
           error: true,
           code: 404,
@@ -40,135 +41,21 @@ class CiudadService {
         };
       }
       // Consultamos los usuarios asociados a la ciudad
-      const usuarios = await ciudadInstance.usuarios(id);
-      // Agregamos la propiedad usuarios al objeto ciudad
-      ciudad.usuarios = usuarios;
+      const usuarios = await ciudadInstance.ciudades(id);
+      // Agregamos la propiedad usuarios al objeto ciudades
+      ciudades.usuarios = usuarios;
       // Retornamos la ciudad obtenida
       return {
         error: false,
         code: 200,
         message: "CIUDAD OBTENIDA CORRECTAMENTE",
-        data: ciudad,
+        data: ciudades,
       };
     } catch (error) {
       return {
         error: true,
         code: 500,
         message: "ERROR: AL OBTENER LA CIUDAD",
-      };
-    }
-  }
-  static async createCity(nombre_ciudad)
-  {
-    try {
-      const ciudadInstance = new Ciudad();
-      const ciudad = await ciudadInstance.create(nombre_ciudad);
-      // Validamos si no se pudo crear la categoría
-      if (ciudad === null) {
-        return {
-          error: true,
-          code: 400,
-          message: "ERROR AL CREAR LA CIUDAD",
-        };
-      }
-      // Retornamos la nueva ciudad creada
-      return {
-        error: false,
-        code: 201,
-        message: "CIUDAD CREADA CORRECTAMENTE",
-        data: ciudad,
-      };
-    } catch (error) {
-      return {
-        error: true,
-        code: 500,
-        message: "ERROR INTERNO AL CREAR LA CIUDAD",
-      };
-    }
-  }
-  static async updateCity(id, campos)
-  {
-    try {
-      const ciudadInstance = new Ciudad();
-      // Consultamos la ciudad por id
-      const ciudadExistente = await ciudadInstance.getById(id);
-      // Validamos si no existe la ciudad
-      if (ciudadExistente.length === 0) {
-        return {
-          error: true,
-          code: 404,
-          message: "CIUDAD NO ENCONTRADA",
-        };
-      }
-      const ciudad = await ciudadInstance.update(id, campos);
-      // Validamos si no se pudo actualizar la ciudad
-      if (ciudad === null) {
-        return{
-          error: true,
-          code: 400,
-          message: "ERROR AL ACTUALIZAR LA CIUDAD",
-        };
-      }
-      // Retornamos la ciudad actualizada
-      return {
-        error: false,
-        code: 200,
-        message: "CIUDAD ACTUALIZADA CORRECTAMENTE",
-        data: ciudad,
-      };
-    } catch (error) {
-      return {
-        error: true,
-        code: 500,
-        message: "ERROR INTERNO AL ACTUALIZAR LA CIUDAD",
-      };
-    }
-  }
-  static async deleteCity(id) {
-    try {
-      const ciudadInstance = new Ciudad();
-      // Consultamos la ciudad por id
-      const ciudadExistente = await ciudadInstance.getById(id);
-      // Validamos si no existe la ciudad
-      if (ciudadExistente.length === 0) {
-        return {
-          error: true,
-          code: 404,
-          message: "CIUDAD NO ENCONTRADA",
-        };
-      }
-      // Consultamos las ciudades asociados a un usuario
-      const ciudades = await ciudadInstance.usuarios(id);
-      // Validamos si no se pudo actualizar la ciudad
-      if (ciudades.length > 0) {
-        return {
-          error: true,
-          code: 404,
-          message: "NO SE PUEDE ELIMINAR LA CIUDAD, TIENE UNO O VARIOS USUARIOS ASOCIADOS.",
-        };
-      }
-      // Procedemos a eliminar la ciudad
-      const resultado = await ciudadInstance.delete(id);
-      // Validamos si no se pudo eliminar la ciudad
-      if (resultado.error) {
-        return {
-          error: true,
-          code: 400,
-          message: resultado.mensaje,
-        };
-      }
-      // Retornamos la respuesta de eliminación
-      return {
-        error: false,
-        code: 200,
-        message: "CIUDAD ELIMINADA CORRECTAMENTE",
-        data: ciudadExistente,
-      };
-    } catch (error) {
-      return {
-        error: true,
-        code: 500,
-        message: "ERROR INTERNO AL ELIMINAR LA CIUDAD",
       };
     }
   }
