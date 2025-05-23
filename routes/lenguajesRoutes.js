157f8099f38c08ddf4cc75f1d176d00b0ca86f4e
Wili Delgado <wilsondelgadomoreno@gmail.com>
2025-05-22 23:03:35 -0500

    creamos models lenguanjes  y su respectivo controllador

--- routes/lenguajesRoutes.js
+++ routes/lenguajesRoutes.js
@@ -1,5 +1,10 @@
 import express from "express";
-//
-//
-const router = express.router();
-module.exports = router;
+import LenguajesController from "../controller/lenguajesController.js";
+
+const router = express.Router();
+router.get('/', LenguajesController.getAllLenguajes);
+router.post('/', LenguajesController.createLenguajes);
+router.put('/:id_lenguaje', LenguajesController.updateLenguajes);
+router.patch('/:id_lenguaje', LenguajesController.updateParcialLenguajes);
+router.delete('/:id_lenguaje', LenguajesController.deleteLenguajes);
+export default router;
